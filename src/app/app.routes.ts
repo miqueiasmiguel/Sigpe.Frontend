a63dab3225f12b3cb00b6dc917737d54690bda6b
Miquéias Miguel <miqueias.s.filho@gmail.com>
2024-03-22 16:16:16 -0300

    modificando nomenclatura de login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './modules/home/home.component';
+import { LoginComponent } from './modules/login/login.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: HomeComponent
+    component: LoginComponent
   }
 ];
