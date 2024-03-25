7ffe6d8b51b9a6edfd20de5aecebdacc8a71c72c
Miquéias Miguel <miqueias.s.filho@gmail.com>
2024-03-24 22:57:48 -0300

    Criando lista de agendamentos

--- src/app/modules/agendamento-lista/agendamento-lista.component.ts
+++ src/app/modules/agendamento-lista/agendamento-lista.component.ts
@@ -1,14 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NavbarComponent } from '../shared/navbar/navbar.component';
+import { AgendamentoService } from '../../services/agendamento.service';
+import { Agendamento } from '../../models/agendamento.interface';
+import { TokenService } from '../../services/token.service';
+import { TipoUsuarioEnum } from '../../enums/tipo-usuario.enum';
+import { CommonModule } from '@angular/common';
+import { StatusAgendamentoEnum } from '../../enums/status-agendamento.enum';
 
 @Component({
   selector: 'app-agendamento-lista',
   standalone: true,
-  imports: [RouterLink, NavbarComponent],
+  imports: [CommonModule, RouterLink, NavbarComponent],
   templateUrl: './agendamento-lista.component.html',
   styleUrl: './agendamento-lista.component.scss'
 })
-export class AgendamentoListaComponent {
+export class AgendamentoListaComponent implements OnInit {
+  private agendamentoService = inject(AgendamentoService);
+  private tokenService = inject(TokenService);
 
+  public statusAgendamento = StatusAgendamentoEnum;
+
+  public showAlert: boolean = false;
+  public alertClass: string = '';
+  public alertMessage: string = '';
+
+  public agendamentos: Agendamento[] = [];
+
+  ngOnInit(): void {
+    this.alimentarLista();
+  }
+
+  private alimentarLista(): void {
+    const tipoUsuario = this.tokenService.getUserRole();
+
+    if (tipoUsuario == TipoUsuarioEnum.RECEPCIONISTA) {
+      this.agendamentoService.get().subscribe({
+        next: (agendamentos) => {
+          this.agendamentos = agendamentos
+        },
+        error: (err) => {
+          this.mostrarAlerta(`Erro: ${err.error.message}`, false);
+        }
+      })
+    }
+  }
+
+  private mostrarAlerta(mensagem: string, isSuccess: boolean) {
+    this.alertClass = isSuccess ? 'alert-success' : 'alert-danger';
+    this.alertMessage = mensagem;
+    this.showAlert = true;
+  }
 }
